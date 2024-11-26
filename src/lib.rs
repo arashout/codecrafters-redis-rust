c356ff4b6c932433225ad84088cb8dd77cb6842b
Arash Outadi <arash.out@gmail.com>
2024-11-26 11:41:53 -0800

    Threadpool

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,67 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+#[derive(Debug, Clone)]
+pub enum PoolCreationError {
+    ZeroSize,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        match size {
+            0 => Err(PoolCreationError::ZeroSize),
+            _ => Ok(ThreadPool::new(size)),
+        }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+
+        self.sender
+            .send(job)
+            .expect("failed to send job to worker pool");
+    }
+}
+
+pub struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker { id, thread }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
     
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::build(4).expect("Failed to build thread pool");
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
